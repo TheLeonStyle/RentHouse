e65d9f04befb9c4c3d85ee36c8755f8c6cbf2566
TheLeonStyle <kirzim2007@gmail.com>
2025-06-17 18:28:25 +0400

    Галерея на главной

---
+++ src/lib/components/home/Gallery.svelte
@@ -0,0 +1,120 @@
+<script>
+	const categories = ['Все', 'Дом снаружи', 'Дом внутри'];
+	let categoryIndex = $state(0);
+
+	const handleCategoryClick = (index) => (categoryIndex = index);
+</script>
+
+<section class="gallery">
+	<div class="gallery__container">
+		<h2 class="gallery__text text">Фотогалерея</h2>
+
+		<nav class="gallery__categories">
+			{#each categories as category, index}
+				<button
+					class="gallery__category"
+					class:active={categoryIndex === index}
+					onclick={() => handleCategoryClick(index)}>{category}</button
+				>
+			{/each}
+		</nav>
+
+		<ul class="gallery__items">
+			<li class="gallery__item">
+				<img src="http://placeskull.com/500/500" alt="" class="gallery__image" loading="lazy" />
+			</li>
+			<li class="gallery__item">
+				<img src="http://placeskull.com/800/500" alt="" class="gallery__image" loading="lazy" />
+			</li>
+			<li class="gallery__item">
+				<img src="http://placeskull.com/500/700" alt="" class="gallery__image" loading="lazy" />
+			</li>
+			<li class="gallery__item">
+				<img src="http://placeskull.com/300/100" alt="" class="gallery__image" loading="lazy" />
+			</li>
+			<li class="gallery__item">
+				<img src="http://placeskull.com/1000/500" alt="" class="gallery__image" loading="lazy" />
+			</li>
+			<li class="gallery__item">
+				<img src="http://placeskull.com/500/200" alt="" class="gallery__image" loading="lazy" />
+			</li>
+		</ul>
+	</div>
+</section>
+
+<style lang="scss">
+	.gallery {
+		/* .gallery__container */
+		&__container {
+		}
+		/* .gallery__text */
+		&__text {
+			// margin-bottom: rem(40);
+			@include adaptiveValue('margin-bottom', 40, 10, 767, 320, 1);
+		}
+		/* .gallery__categories */
+		&__categories {
+			display: flex;
+			justify-content: center;
+			flex-wrap: wrap;
+			// gap: rem(40);
+			@include adaptiveValue('gap', 40, 20, 767, 320, 1);
+
+			// margin-bottom: rem(30);
+			@include adaptiveValue('margin-bottom', 30, 20, 767, 320, 1);
+		}
+		/* .gallery__category */
+		&__category {
+			color: #ffffff;
+			// font-size: rem(24);
+			@include adaptiveValue('font-size', 24, 16, 767, 320, 1);
+			font-weight: 500;
+			line-height: math.div(30, 24);
+			position: relative;
+
+			&::after {
+				content: '';
+				position: absolute;
+				bottom: 0;
+				left: 0;
+
+				width: 0;
+				height: rem(2);
+				background-color: #2b7af1;
+				transition: width 0.3s ease 0s;
+			}
+
+			&:hover,
+			&.active {
+				&::after {
+					width: 100%;
+				}
+			}
+		}
+		/* .gallery__items */
+		&__items {
+			display: grid;
+			grid-template-columns: repeat(3, 1fr);
+			gap: rem(10);
+			@media (max-width: $mobile) {
+				grid-template-columns: repeat(2, 1fr);
+			}
+			@media (max-width: $mobileSmall) {
+				grid-template-columns: repeat(1, 1fr);
+			}
+		}
+		/* .gallery__item */
+		&__item {
+			// height: rem(260);
+			@include adaptiveValue('height', 260, 200, 991, 767, 1);
+			border-radius: rem(20);
+			overflow: hidden;
+		}
+		/* .gallery__image */
+		&__image {
+			width: 100%;
+			height: 100%;
+			object-fit: cover;
+		}
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+	import Gallery from '$lib/components/home/Gallery.svelte';
+</script>
+
+<Gallery />
