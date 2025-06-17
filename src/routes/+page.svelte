935ba2e9e5ce3ba7016e8ed1d5fc067ce29895ab
TheLeonStyle <kirzim2007@gmail.com>
2025-06-17 19:34:08 +0400

    Контакты на главной

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script>
 	import Gallery from '$lib/components/home/Gallery.svelte';
+	import Contacts from '$lib/components/home/Contacts.svelte';
 </script>
 
-<Gallery />
+<!-- <Gallery /> -->
+<!-- <Contacts /> -->
